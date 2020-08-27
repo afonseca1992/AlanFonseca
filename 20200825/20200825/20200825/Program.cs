ae519e8a13889e0a0e67f701f8411a500d2e3434
AlanFonseca <alan.querol@davinci.edu.ar>
2020-08-27 18:54:55 -0300

    Ejercicio Alan Fonseca 20200825

---
+++ 20200825/20200825/20200825/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _20200825
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intentos = 1;
+            String clave = "";
+            while (clave != "secreto" && intentos < 4)
+            {
+                Console.Write("Ingresa la contraseña");
+                clave = Console.ReadLine();
+     
+                intentos++;
+            }
+        }
+    }
+}
---
+++ 20200825/20200825/Ejercicio20200825/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio20200825
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            string userinput = "";
+            do
+            {
+                Console.WriteLine("Ingresa el numero");
+                userinput = Console.ReadLine();
+            }
+            while (x != int.Parse(userinput));
+            Console.WriteLine("Adivinaste");
+        }
+    }
+}
