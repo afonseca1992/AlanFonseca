62997ed743272f392636b1d90088132e78cf7a19
AlanFonseca <alan.querol@davinci.edu.ar>
2020-09-01 22:37:46 -0300

    ejercicio random

---
+++ 20200901/20200901/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _20200901
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Random random = new Random()
+                int y = 0;
+                int x = random.Next(10);
+                string userinput = "";
+                do
+                {
+                    Console.WriteLine("Ingresa el numero");
+                    userinput = Console.ReadLine();
+                }
+                while (x != int.Parse(userinput) or y = 3);
+                Console.WriteLine("Adivinaste");
+                    y++;
+            }
+        }
+    }
+}
+
